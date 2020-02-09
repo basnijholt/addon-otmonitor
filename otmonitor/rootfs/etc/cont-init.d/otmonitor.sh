9946030be27568d74bbacd3b4c9181cdc6178d98
Bas Nijholt <basnijholt@gmail.com>
2020-02-09 22:56:23 +0100

    use s6 properly

--- otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
+++ otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
@@ -1,13 +1,13 @@
-CONFIG_PATH=/data/options.json
 
+bashio::log.info "Initializing the config overwriting."
 OTMONITOR_CONF=/etc/otmonitor/otmonitor.conf
 
-otgw_host=$(jq --raw-output ".otgw_host" $CONFIG_PATH)
-otgw_port=$(jq --raw-output ".otgw_port" $CONFIG_PATH)
-mqtt_broker=$(jq --raw-output ".mqtt_broker" $CONFIG_PATH)
-mqtt_port=$(jq --raw-output ".mqtt_port" $CONFIG_PATH)
-mqtt_username=$(jq --raw-output ".mqtt_username" $CONFIG_PATH)
-mqtt_password=$(jq --raw-output ".mqtt_password" $CONFIG_PATH)
+otgw_host=$(bashio::config "otgw_host")
+otgw_port=$(bashio::config "otgw_port")
+mqtt_broker=$(bashio::config "mqtt_broker")
+mqtt_port=$(bashio::config "mqtt_port")
+mqtt_username=$(bashio::config "mqtt_username")
+mqtt_password=$(bashio::config "mqtt_password")
 
 mqtt_password_base64=`echo -n ${mqtt_password} | base64`
 
@@ -18,4 +18,4 @@ sed -i 's/%%mqtt_port%%/${mqtt_port}/g' ${OTMONITOR_CONF}
 sed -i 's/%%mqtt_username%%/${mqtt_username}/g' ${OTMONITOR_CONF}
 sed -i 's/%%mqtt_password%%/${mqtt_password_base64}/g' ${OTMONITOR_CONF}
 
-/usr/bin/otmonitor --daemon -f ${OTMONITOR_CONF}
+bashio::log.info "Finished the config overwriting."
