107c9c3af29e259aab97cd56878087e21f63e624
Bas Nijholt <basnijholt@gmail.com>
2020-02-09 22:21:40 +0100

    move config and use base64

--- otmonitor/run.sh
+++ otmonitor/run.sh
@@ -1,6 +1,6 @@
 CONFIG_PATH=/data/options.json
 
-OTMONITOR_CONF=/data/otmonitor.conf
+OTMONITOR_CONF=/etc/otmonitor/otmonitor.conf
 
 otgw_host=$(jq --raw-output ".otgw_host" $CONFIG_PATH)
 otgw_port=$(jq --raw-output ".otgw_port" $CONFIG_PATH)
@@ -9,11 +9,13 @@ mqtt_port=$(jq --raw-output ".mqtt_port" $CONFIG_PATH)
 mqtt_username=$(jq --raw-output ".mqtt_username" $CONFIG_PATH)
 mqtt_password=$(jq --raw-output ".mqtt_password" $CONFIG_PATH)
 
+mqtt_password_base64=`echo -n ${mqtt_password} | base64`
+
 sed -i 's/otgw_host/${otgw_host}/g' ${OTMONITOR_CONF}
 sed -i 's/otgw_port/${otgw_port}/g' ${OTMONITOR_CONF}
 sed -i 's/mqtt_broker/${mqtt_broker}/g' ${OTMONITOR_CONF}
 sed -i 's/mqtt_port/${mqtt_port}/g' ${OTMONITOR_CONF}
 sed -i 's/mqtt_username/${mqtt_username}/g' ${OTMONITOR_CONF}
-sed -i 's/mqtt_password/${mqtt_password}/g' ${OTMONITOR_CONF}
+sed -i 's/mqtt_password/${mqtt_password_base64}/g' ${OTMONITOR_CONF}
 
 /app/otmonitor --daemon -f ${OTMONITOR_CONF}
