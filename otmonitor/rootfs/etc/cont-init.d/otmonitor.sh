3507857c61328b06fa78686af6356487ac5f90dc
Bas Nijholt <basnijholt@gmail.com>
2020-02-10 00:08:25 +0100

    use double quotes

--- otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
+++ otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
@@ -16,11 +16,11 @@ mqtt_username=$(bashio::config "mqtt_username")
 mqtt_password=$(bashio::config "mqtt_password")
 mqtt_password_base64=`echo -n $mqtt_password | base64`
 
-sed -i "s#%%otgw_host%%#${otgw_host}#g" ${OTMONITOR_CONF}
-sed -i "s#%%otgw_port%%#${otgw_port}#g" ${OTMONITOR_CONF}
-sed -i "s#%%mqtt_broker%%#${mqtt_broker}#g" ${OTMONITOR_CONF}
-sed -i "s#%%mqtt_port%%#${mqtt_port}#g" ${OTMONITOR_CONF}
-sed -i "s#%%mqtt_username%%#${mqtt_username}#g" ${OTMONITOR_CONF}
-sed -i "s#%%mqtt_password%%#${mqtt_password_base64}#g" ${OTMONITOR_CONF}
+sed -i "s/%%otgw_host%%/${otgw_host}/g" ${OTMONITOR_CONF}
+sed -i "s/%%otgw_port%%/${otgw_port}/g" ${OTMONITOR_CONF}
+sed -i "s/%%mqtt_broker%%/${mqtt_broker}/g" ${OTMONITOR_CONF}
+sed -i "s/%%mqtt_port%%/${mqtt_port}/g" ${OTMONITOR_CONF}
+sed -i "s/%%mqtt_username%%/${mqtt_username}/g" ${OTMONITOR_CONF}
+sed -i "s/%%mqtt_password%%/${mqtt_password_base64}/g" ${OTMONITOR_CONF}
 
 bashio::log.info "Finished the config overwriting."
