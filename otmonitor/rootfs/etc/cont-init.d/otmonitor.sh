a09df7e5ded519b1678d17f7c1308df242c4f5e6
Andrzej <andriej@users.noreply.github.com>
2020-10-12 22:22:56 +0200

    Update otmonitor.sh

--- otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
+++ otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
@@ -10,6 +10,7 @@ OTMONITOR_CONF=/etc/otmonitor/otmonitor.conf
 
 otgw_host=$(bashio::config "otgw_host")
 otgw_port=$(bashio::config "otgw_port")
+relay_port=$(bashio::config "relay_port")
 mqtt_broker=$(bashio::config "mqtt_broker")
 mqtt_port=$(bashio::config "mqtt_port")
 mqtt_username=$(bashio::config "mqtt_username")
@@ -18,6 +19,7 @@ mqtt_password_base64=`echo -n $mqtt_password | base64 -w 0`
 
 sed -i "s/%%otgw_host%%/${otgw_host}/g" ${OTMONITOR_CONF}
 sed -i "s/%%otgw_port%%/${otgw_port}/g" ${OTMONITOR_CONF}
+sed -i "s/%%relay_port%%/${relay_port}/g" ${OTMONITOR_CONF}
 sed -i "s/%%mqtt_broker%%/${mqtt_broker}/g" ${OTMONITOR_CONF}
 sed -i "s/%%mqtt_port%%/${mqtt_port}/g" ${OTMONITOR_CONF}
 sed -i "s/%%mqtt_username%%/${mqtt_username}/g" ${OTMONITOR_CONF}
