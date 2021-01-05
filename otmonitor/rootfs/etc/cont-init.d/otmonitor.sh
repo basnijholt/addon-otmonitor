a21ccf1c4dfa07e4261b191b65f5d1f7f8916ec1
Flip <flip@fliphess.com>
2021-01-05 21:27:44 +0100

    Add the option to do autoconfig using the mosquitto addon

--- otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
+++ otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
@@ -8,21 +8,34 @@ bashio::log.info "Initializing the config overwriting."
 
 OTMONITOR_CONF=/etc/otmonitor/otmonitor.conf
 
-otgw_host=$(bashio::config "otgw_host")
-otgw_port=$(bashio::config "otgw_port")
-relay_port=$(bashio::config "relay_port")
-mqtt_broker=$(bashio::config "mqtt_broker")
-mqtt_port=$(bashio::config "mqtt_port")
-mqtt_username=$(bashio::config "mqtt_username")
-mqtt_password=$(bashio::config "mqtt_password")
-mqtt_password_base64=`echo -n $mqtt_password | base64 -w 0`
+otgw_host="$( bashio::config 'otgw_host')"
+otgw_port="$( bashio::config 'otgw_port')"
+relay_port="$( bashio::config 'relay_port')"
 
-sed -i "s/%%otgw_host%%/${otgw_host}/g" ${OTMONITOR_CONF}
-sed -i "s/%%otgw_port%%/${otgw_port}/g" ${OTMONITOR_CONF}
-sed -i "s/%%relay_port%%/${relay_port}/g" ${OTMONITOR_CONF}
-sed -i "s/%%mqtt_broker%%/${mqtt_broker}/g" ${OTMONITOR_CONF}
-sed -i "s/%%mqtt_port%%/${mqtt_port}/g" ${OTMONITOR_CONF}
-sed -i "s/%%mqtt_username%%/${mqtt_username}/g" ${OTMONITOR_CONF}
-sed -i "s/%%mqtt_password%%/${mqtt_password_base64}/g" ${OTMONITOR_CONF}
+if bashio::config 'mqtt_autoconfig' ; then
+    bashio::log.info "Using autoconfig provided MQTT credentials from supervisor:"
+
+    mqtt_broker="$( bashio::services mqtt 'host' )"
+    mqtt_port="$( bashio::services mqtt 'port' )"
+    mqtt_username="$( bashio::services mqtt 'username' )"
+    mqtt_password="$( bashio::services mqtt 'password' )"
+else
+    bashio::log.info "Using manually p	rovided MQTT credentials"
+
+    mqtt_broker="$( bashio::config 'mqtt_broker' )"
+    mqtt_port="$( bashio::config 'mqtt_port' )"
+    mqtt_username="$( bashio::config 'mqtt_username' )"
+    mqtt_password="$( bashio::config 'mqtt_password' )"
+fi
+
+mqtt_password_base64="$( echo -n "${mqtt_password}" | base64 -w 0 )"
+
+sed -i "s|%%otgw_host%%|${otgw_host}|g" ${OTMONITOR_CONF}
+sed -i "s|%%otgw_port%%|${otgw_port}|g" ${OTMONITOR_CONF}
+sed -i "s|%%relay_port%%|${relay_port}|g" ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_broker%%|${mqtt_broker}|g" ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_port%%|${mqtt_port}|g" ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_username%%|${mqtt_username}|g" ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_password%%|${mqtt_password_base64}|g" ${OTMONITOR_CONF}
 
 bashio::log.info "Finished the config overwriting."
