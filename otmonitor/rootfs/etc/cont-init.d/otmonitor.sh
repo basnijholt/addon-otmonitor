b4d5e6f06250963e77d04647f47f88426eeb7740
Flip <flip@fliphess.com>
2021-01-12 17:47:57 +0100

    Shuffle some parts around

--- otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
+++ otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
@@ -8,9 +8,22 @@ bashio::log.info "Initializing the config overwriting."
 
 OTMONITOR_CONF=/etc/otmonitor/otmonitor.conf
 
+# ======================================
+# Update otgw host and ports in config
+# ======================================
+
 otgw_host="$( bashio::config 'otgw_host')"
 otgw_port="$( bashio::config 'otgw_port')"
 relay_port="$( bashio::config 'relay_port')"
+
+sed -i "s|%%otgw_host%%|${otgw_host}|g" ${OTMONITOR_CONF}
+sed -i "s|%%otgw_port%%|${otgw_port}|g" ${OTMONITOR_CONF}
+sed -i "s|%%relay_port%%|${relay_port}|g" ${OTMONITOR_CONF}
+
+
+# ======================================
+# Update MQTT settings in config
+# ======================================
 
 if bashio::config 'mqtt_autoconfig' ; then
     bashio::log.info "Using autoconfig provided MQTT credentials from supervisor"
@@ -28,21 +41,18 @@ else
     mqtt_password="$( bashio::config 'mqtt_password' )"
 fi
 
-## b64 encode the password
 mqtt_password_base64="$( echo -n "${mqtt_password}" | base64 -w 0 )"
-
-## Set the client_id
 mqtt_client_id="$( bashio::config 'mqtt_client_id' )"
+
 if bashio::var.has_value "${mqtt_client_id}" ; then
-    sed -i "s|random_identifier_here|${mqtt_client_id}|g" ${OTMONITOR_CONF}
+    sed -i "s|random_identifier_here|${mqtt_client_id:-'otmonitor'}|g" ${OTMONITOR_CONF}
 fi
 
-sed -i "s|%%otgw_host%%|${otgw_host}|g" ${OTMONITOR_CONF}
-sed -i "s|%%otgw_port%%|${otgw_port}|g" ${OTMONITOR_CONF}
-sed -i "s|%%relay_port%%|${relay_port}|g" ${OTMONITOR_CONF}
 sed -i "s|%%mqtt_broker%%|${mqtt_broker}|g" ${OTMONITOR_CONF}
 sed -i "s|%%mqtt_port%%|${mqtt_port}|g" ${OTMONITOR_CONF}
 sed -i "s|%%mqtt_username%%|${mqtt_username}|g" ${OTMONITOR_CONF}
 sed -i "s|%%mqtt_password%%|${mqtt_password_base64}|g" ${OTMONITOR_CONF}
+
+
 
 bashio::log.info "Finished the config overwriting."
