94838fc4fe52680e77ed0764fa18600d509b0a6d
Flip <flip@fliphess.com>
2021-01-23 17:21:05 +0100

    Update otmonitor.conf: add mqtt event and action topic

--- otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
+++ otmonitor/rootfs/etc/cont-init.d/otmonitor.sh
@@ -42,8 +42,8 @@ otgw_host="$( bashio::config 'otgw_host' )"
 otgw_port="$( bashio::config 'otgw_port' )"
 relay_port="$( bashio::config 'relay_port' )"
 
-sed -i "s|%%otgw_host%%|${otgw_host}|g" ${OTMONITOR_CONF}
-sed -i "s|%%otgw_port%%|${otgw_port}|g" ${OTMONITOR_CONF}
+sed -i "s|%%otgw_host%%|${otgw_host}|g"   ${OTMONITOR_CONF}
+sed -i "s|%%otgw_port%%|${otgw_port}|g"   ${OTMONITOR_CONF}
 sed -i "s|%%relay_port%%|${relay_port}|g" ${OTMONITOR_CONF}
 
 
@@ -51,7 +51,7 @@ sed -i "s|%%relay_port%%|${relay_port}|g" ${OTMONITOR_CONF}
 # Update MQTT settings in config
 # ======================================
 
-if bashio::config.true 'mqtt_autoconfig' ; then
+if bashio::config.true 'mqtt.autoconfig' ; then
     bashio::log.info "Using autoconfig provided MQTT credentials from supervisor"
 
     mqtt_broker="$( bashio::services mqtt 'host' )"
@@ -61,22 +61,23 @@ if bashio::config.true 'mqtt_autoconfig' ; then
 else
     bashio::log.info "Using manually provided MQTT credentials"
 
-    mqtt_broker="$( bashio::config 'mqtt_broker' )"
-    mqtt_port="$( bashio::config 'mqtt_port' )"
-    mqtt_username="$( bashio::config 'mqtt_username' )"
-    mqtt_password="$( bashio::config 'mqtt_password' )"
+    mqtt_broker="$( bashio::config 'mqtt.broker' )"
+    mqtt_port="$( bashio::config 'mqtt.port' )"
+    mqtt_username="$( bashio::config 'mqtt.username' )"
+    mqtt_password="$( bashio::config 'mqtt.password' )"
 fi
 
+mqtt_client_id="$( bashio::config 'mqtt.client_id' )"
+mqtt_action_topic="$( bashio::config 'mqtt.action_topic' )"
+mqtt_event_topic="$( bashio::config 'mqtt.event_topic' )"
 mqtt_password_base64="$( echo -n "${mqtt_password}" | base64 -w 0 )"
-mqtt_client_id="$( bashio::config 'mqtt_client_id' )"
 
-if bashio::var.has_value "${mqtt_client_id}" ; then
-    sed -i "s|random_identifier_here|${mqtt_client_id:-'otmonitor'}|g" ${OTMONITOR_CONF}
-fi
+sed -i "s|%%mqtt_broker%%|${mqtt_broker}|g"             ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_port%%|${mqtt_port}|g"                 ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_username%%|${mqtt_username}|g"         ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_password%%|${mqtt_password_base64}|g"  ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_client_id%%|${mqtt_client_id}|g"       ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_event_topic%%|${mqtt_event_topic}|g"   ${OTMONITOR_CONF}
+sed -i "s|%%mqtt_action_topic%%|${mqtt_action_topic}|g" ${OTMONITOR_CONF}
 
-sed -i "s|%%mqtt_broker%%|${mqtt_broker}|g" ${OTMONITOR_CONF}
-sed -i "s|%%mqtt_port%%|${mqtt_port}|g" ${OTMONITOR_CONF}
-sed -i "s|%%mqtt_username%%|${mqtt_username}|g" ${OTMONITOR_CONF}
-sed -i "s|%%mqtt_password%%|${mqtt_password_base64}|g" ${OTMONITOR_CONF}
-
-bashio::log.info "Finished the config overwriting."
+bashio::log.info "Finished updating otmonitor config file."
